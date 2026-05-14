dee566f4bfcf1aa3b364aa4e7d3d31ae0e469712
Giovanag0 <giovana.g.santos@ba.estudante.senai.br>
2026-05-14 16:19:31 -0300

    Index finalizado com botões, carrossel, enquadramento

--- js/carousel.js
+++ js/carousel.js
@@ -8,25 +8,63 @@ let carouselArr = [];
 
 //class Carousel
 class Carousel {
-
-    
+  constructor(image, title, url) {
+    this.image = image;
+    this.title = title;
+    this.url = url
+  }
       
     static Start(arr){
-        if(arr){
 
-            if(arr.length > 0){
+            if(arr && arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
+                Carousel.Update();
+                Carousel.ResetTimer();
+            } else {
             throw "Method Start need a Array Variable.";
         }
     }
 
-    static Next(){
-        
+    static ResetTimer(){
+        if(Carousel._interval) clearInterval(Carousel._interval);
+            Carousel._interval = setInterval(function(){
+                Carousel.Next();
+        }, 5000);
     }
-};
+
+   static Next() {
+        Carousel._sequence++;
+        if (Carousel._sequence >= Carousel._size) {
+            Carousel._sequence = 0;
+        }
+        Carousel.Update();
+        Carousel.ResetTimer();
+    }
+
+static Prev() {
+        Carousel._sequence--;
+        if (Carousel._sequence < 0) {
+            Carousel._sequence = Carousel._size - 1;
+        }
+        Carousel.Update();
+        Carousel.ResetTimer();
+    }
+
+static Update() {
+        let obj = carouselArr[Carousel._sequence];
+        const divImg = document.getElementById("carousel");
+
+        if (divImg) {
+            divImg.style.backgroundImage = `url('img/${obj.image}')`;
+            divImg.style.backgroundSize = "contain";
+            divImg.style.backgroundRepeat = "no-repeat"; 
+            divImg.style.backgroundPosition = "center";
+        }
+
+      const divTitle = document.getElementById("carousel-title");
+      if (divTitle){
+        divTitle.innerHTML = `<a href="${obj.url}">${obj.title}</a>`;
+      }
+    }
+}
